56d9c03eba1c72451cda477e10497af245179290
Farrux <Farrux@gmail.com>
2025-11-07 07:06:15 +0500

    Enhance header modal functionality and styles; add hero section to HTML

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-const headerModal = document.querySelector(".header__modal")
-const barsIcon = document.getElementById("hiddenIcon");
+const headerModal = document.querySelector(".header__modal");
+const barsBtnIcon = document.getElementById("hiddenIcon");
+const closeBtnIcon = document.getElementById("closeBtn");
 
+barsBtnIcon.addEventListener("click", () => {
+  headerModal.style.display = "flex";
+  headerModal.classList.add("active");
+  barsIcon.style.display = "none";
+});
+
+closeBtnIcon.addEventListener("click", () => {
+  headerModal.style.display = "none";
+});
